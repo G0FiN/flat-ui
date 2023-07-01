3985b9d382e6e884bad4daa0fb4c7d525dc97514
Serhii Vodianov <vodpbd4@gmail.com>
2023-07-01 17:32:59 +0300

    add palettes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 import colorData from './colors.json';
 
 const ColorCard = ({ name, color }) => (
@@ -9,11 +10,30 @@ const ColorCard = ({ name, color }) => (
 );
 
 function App() {
+  const [selectedPalette, setSelectedPalette] = useState(0);
+
+  const handlePaletteChange = (index) => {
+    setSelectedPalette(index);
+  };
+
+  const selectedPaletteData = colorData[selectedPalette];
+
   return (
-    <div className="App">
+    <div className="app">
       <h1>Color Cards</h1>
+      <div className="palette-buttons">
+        {colorData.map((palette, index) => (
+          <button
+            key={palette.id}
+            className={selectedPalette === index ? 'active' : ''}
+            onClick={() => handlePaletteChange(index)}
+          >
+            {palette.emoji}
+          </button>
+        ))}
+      </div>
       <div className="color-cards">
-        {colorData.colors.map((color) => (
+        {selectedPaletteData.colors.map((color) => (
           <ColorCard key={color.name} name={color.name} color={color.color} />
         ))}
       </div>
