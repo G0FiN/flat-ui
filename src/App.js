62fa53d51bd17e46b7c3a63639f9a05895fc4aa5
Serhii Vodianov <vodpbd4@gmail.com>
2023-07-01 17:26:45 +0300

    Add cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import colorData from './colors.json';
+
+const ColorCard = ({ name, color }) => (
+  <div className="color-card" style={{ backgroundColor: color }}>
+    <h3>{name}</h3>
+    <p>{color}</p>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Color Cards</h1>
+      <div className="color-cards">
+        {colorData.colors.map((color) => (
+          <ColorCard key={color.name} name={color.name} color={color.color} />
+        ))}
+      </div>
     </div>
   );
 }
